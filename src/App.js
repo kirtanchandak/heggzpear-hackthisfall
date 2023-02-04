ee19cf7d1b6cf2640b76ec54c7a9dd68ed8f07f8
kirtanchandak <themarketingkirtan@gmail.com>
2023-02-04 10:44:58 +0530

    input form structure

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Dashboard from "./components/Dashboard";
+import Form from "./components/Form";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/form" element={<Form />} />
       </Routes>
     </BrowserRouter>
   );
